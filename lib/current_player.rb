77c6a3a58611ee45c2d3b73a5fc57df8259588cd
Ivan Szebenszki <szib77@gmail.com>
2019-03-04 18:52:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,9 @@
 def turn_count(board)
   taken = 0
   board.each {|t| taken += 1 if t != " "}
-  return taken
+  taken
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
